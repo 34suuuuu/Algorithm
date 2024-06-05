f35e33a657035081a3db5cc37309789cba79696b
SuHyunPark <80260557+34suuuuu@users.noreply.github.com>
2024-06-05 11:07:52 +0900

    [Silver I] Title: 영역 구하기, Time: 120 ms, Memory: 15244 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -1,11 +1,14 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	static int N,M, K;
 	static int[][] maps;
-	static boolean[][] visited;
 	static int cnt;
 
 	static int[] dx = {0, 0, -1, 1};
@@ -15,11 +18,11 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		M = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 
-		maps = new int[M][N];
+		maps = new int[N][M];
 		ArrayList<Integer> result = new ArrayList<>();
 
 		for (int i = 0; i < K; i++) {
@@ -32,8 +35,8 @@ public class Main {
 			makeRec(x1, y1, x2, y2);
 		}
 
-		for (int i = 0; i < M; i++) {
-			for (int j = 0; j < N; j++) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
 				if (maps[i][j] == 0) {
 					cnt = 1;
 					bfs(i, j);
@@ -54,25 +57,25 @@ public class Main {
 
 	// 입력받은 직사각형 구역을 1로 처리
 	static void makeRec(int x1, int y1, int x2, int y2) {
-		for (int y = y1; y < y2; y++) {
-			for (int x = x1; x < x2; x++) {
+		for (int x = x1; x < x2; x++) {
+			for (int y = y1; y < y2; y++) {
 				maps[y][x] = 1;
 			}
 		}
 
 	}
 
-	static void bfs(int y, int x) {
-		maps[y][x] = 1;
+	static void bfs(int x, int y) {
+		maps[x][y] = 1;
 
 		for (int i = 0; i < 4; i++) {
 			int nx = x + dx[i];
 			int ny = y + dy[i];
 
 			if(nx < 0 || nx >=N || ny < 0 || ny >= M) continue;
-			if (maps[ny][nx] == 0) {
+			if (maps[nx][ny] == 0) {
 				cnt++;
-				bfs(ny,nx);
+				bfs(nx,ny);
 			}
 		}
 
