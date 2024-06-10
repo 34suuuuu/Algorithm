fd2af6d98403ff0c520167ce2d48747bfbd032b9
SuHyunPark <80260557+34suuuuu@users.noreply.github.com>
2024-06-10 17:24:35 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.88 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
 
 ### 성능 요약
 
-메모리: 71.9 MB, 시간: 0.75 ms
+메모리: 72.4 MB, 시간: 0.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 10일 11:45:27
+2024년 06월 10일 17:24:33
 
 ### 문제 설명
 
--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -2,27 +2,22 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers) {
-        int[] tmp = new int[numbers.length * numbers.length];
-
-		int idx = 0;
+        List<Integer> result = new ArrayList<>();
 		for (int i = 0; i < numbers.length - 1; i++) {
 			for (int j = i + 1; j < numbers.length; j++) {
-				tmp[idx] = numbers[i] + numbers[j];
-				idx++;
+				int sum = numbers[i] + numbers[j];
+				if (!result.contains(sum)) {
+					result.add(sum);
+				}
 			}
 		}
-		int[] tmp2 = Arrays.copyOf(tmp, idx);
-		Arrays.sort(tmp2);
-
-		idx = 0;
-		int[] tmp3 = new int[tmp2.length];
-		for (int i = 0; i < tmp2.length; i++) {
-			if (i == 0 || tmp2[i] != tmp2[i - 1]) {
-				tmp3[idx] = tmp2[i];
-				idx++;
-			}
+		Collections.sort(result);
+		int[] answer = new int[result.size()];
+		int idx = 0;
+		for (Integer i : result) {
+			answer[idx] = i;
+			idx++;
 		}
-		int[] answer = Arrays.copyOf(tmp3, idx);
         return answer;
     }
 }
