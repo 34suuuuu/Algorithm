e4232f1adacb4da18962e00ae62f5fbddd95cae4
SuHyunPark <80260557+34suuuuu@users.noreply.github.com>
2024-06-11 15:25:56 +0900

    [level 2] Title: 전화번호 목록, Time: 340.96 ms, Memory: 202 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,14 +1,17 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
-
-		for (int i = 0; i < phone_book.length-1; i++) {
-			if (phone_book[i + 1].startsWith(phone_book[i])) {
-				return false;
+        Set<String> mySet = new HashSet<>(Arrays.asList(phone_book));
+		boolean answer = true;
+		for (String s : phone_book) {
+			for (int i = 0; i < s.length(); i++) {
+				if (mySet.contains(s.substring(0, i))) {
+					answer = false;
+					break;
+				}
 			}
 		}
-		return true;
+        return answer;
     }
 }
