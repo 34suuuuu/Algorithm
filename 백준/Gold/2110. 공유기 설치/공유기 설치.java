7b314e625a57b421e75013c00c4082fd51ad9311
SuhyunPark <80260557+34suuuuu@users.noreply.github.com>
2024-07-08 23:46:49 +0900

    [Gold IV] Title: 공유기 설치, Time: 308 ms, Memory: 28728 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,49 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N, C;
-    static int[] house;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int[] houses = new int[n];
+		for (int i = 0; i < n; i++) {
+			houses[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(houses);
 
-        house = new int[N];
-        for (int i = 0; i < N; i++) {
-            house[i] = Integer.parseInt(br.readLine());
-        }
-        Arrays.sort(house);
+		int start = 1;
+		int end = houses[n - 1] - houses[0];
+		while (start <= end) {
+			int mid = (start + end) / 2;
 
-        int start = 1;
-        int end = house[N - 1] - house[0];
-        int result = 0;
-
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            int left = house[0];
-            int cnt = 1;
-
-            for (int i = 1; i < N; i++) {
-                if (house[i] - left >= mid) {
-                    cnt++;
-                    left = house[i];
-                }
-            }
-
-            if (cnt >= C) {
-                result = mid;
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        System.out.println(result);
-    }
-
+			int position = 0;
+			int cnt = 1;
+			for (int i = 1; i < n; i++) {
+				if (houses[i] - houses[position] >= mid) {
+					// 최소거리보다 크다면 설치
+					position = i;
+					cnt++;
+				}
+			}
+			if (cnt < c) {
+				// 설치 갯수가 적다면 최소 거리를 줄여주기
+				end = mid - 1;
+				continue;
+			}
+			start = mid + 1;
+		}
+		System.out.println(start - 1);
+	}
 }
