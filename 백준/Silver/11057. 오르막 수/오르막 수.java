ee9e22c9fe525ed2b9ed483be3a92351a3891b0e
SuhyunPark <80260557+34suuuuu@users.noreply.github.com>
2024-07-09 09:29:31 +0900

    [Silver I] Title: 오르막 수, Time: 128 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.java
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -5,16 +5,17 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-        int[][] dp = new int[n + 1][10];
-		Arrays.fill(dp[0], 1);
 
-		for (int i = 1; i <= n; i++) {
+		int[][] dp = new int[n + 1][10];
+		Arrays.fill(dp[1], 1);
+
+		for (int i = 2; i <= n; i++) {
 			for (int j = 0; j < 10; j++) {
 				for (int k = j; k < 10; k++) {
 					dp[i][j] = (dp[i][j] + dp[i - 1][k]) % 10007;
 				}
 			}
 		}
-		System.out.println(dp[n][0]);
+		System.out.println(Arrays.stream(dp[n]).reduce(0, (a, b) -> (a + b) % 10007));;
 	}
 }
