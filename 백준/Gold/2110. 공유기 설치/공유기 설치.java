1ab7fa8e5ba3f1bfd3f94476ceb0ee3d27cf6fbe
박수현 <80260557+34suuuuu@users.noreply.github.com>
2024-11-16 15:07:23 +0900

    [Gold IV] Title: 공유기 설치, Time: 264 ms, Memory: 29152 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,19 +1,23 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
 		int n = Integer.parseInt(st.nextToken());
 		int c = Integer.parseInt(st.nextToken());
+
 		int[] houses = new int[n];
 		for (int i = 0; i < n; i++) {
 			houses[i] = Integer.parseInt(br.readLine());
 		}
 		Arrays.sort(houses);
 
+		// 거리 구해야하니깐 이분탐색으로
 		int start = 1;
 		int end = houses[n - 1] - houses[0];
 		while (start <= end) {
@@ -23,13 +27,12 @@ public class Main {
 			int cnt = 1;
 			for (int i = 1; i < n; i++) {
 				if (houses[i] - houses[position] >= mid) {
-					// 최소거리보다 크다면 설치
 					position = i;
 					cnt++;
 				}
 			}
+
 			if (cnt < c) {
-				// 설치 갯수가 적다면 최소 거리를 줄여주기
 				end = mid - 1;
 				continue;
 			}
