8dcf21b030fb50e7f1b6daf918b23b2911d9dfbf
박수현 <80260557+34suuuuu@users.noreply.github.com>
2024-11-28 12:18:27 +0900

    [Gold IV] Title: 좋다, Time: 156 ms, Memory: 14628 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] nums = new int[n];
+		for (int i = 0; i < n; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(nums);
+
+		int result = 0;
+		for (int i = 0; i < n; i++) {
+			int start = 0;
+			int end = n - 1;
+
+			while (start < end) {
+				if (start == i) {
+					start++;
+				} else if (end == i) {
+					end--;
+				}else{
+					int sum = nums[start] + nums[end];
+					if (sum == nums[i]) {
+						result++;
+						break;
+					} else if (sum < nums[i]) {
+						start++;
+					} else {
+						end--;
+					}
+				}
+			}
+		}
+		System.out.println(result);
+	}
+}
