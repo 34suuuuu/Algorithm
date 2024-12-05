6a711000373c87790ce0b0dc83db23234b7be625
박수현 <80260557+34suuuuu@users.noreply.github.com>
2024-12-05 13:01:15 +0900

    [Silver I] Title: 숨바꼭질, Time: 268 ms, Memory: 100568 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,55 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, K;
-    static Queue<Integer> queue = new LinkedList<>();
-    static int[] check = new int[100001];
+	static int n, k;
+	static int[] dist = new int[2000002];
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
 
-        if (N == K) {
-            System.out.println(0);
-        } else {
-            bfs(N);
-        }
-    }
+		Arrays.fill(dist, -1);
 
-    public static void bfs(int num) {
-        check[num] = 1;
-        queue.add(num);
+		if (n == k) {
+			System.out.println(0);
+			return;
+		}
+		bfs(n);
+	}
 
-        while (!queue.isEmpty()) {
-            int cur = queue.poll();
-            int next;
+	static void bfs(int pos) {
+		Queue<Integer> que = new LinkedList<>();
+		que.add(pos);
+		dist[pos] = 0;
 
-            for (int i = 0; i < 3; i++) {
-                if (i == 0) {
-                    next = cur - 1;
-                } else if (i == 1) {
-                    next = cur + 1;
-                } else {
-                    next = cur * 2;
-                }
+		while (!que.isEmpty()) {
+			int cur = que.poll();
+			int next;
 
-                if (next == K) {
-                    System.out.println(check[cur]);
-                    return;
-                }
-                if (next >= 0 && next < check.length && check[next] == 0) {
-                    queue.add(next);
-                    check[next] = check[cur] + 1;
-                }
-            }
-        }
-    }
+			if (cur == k) {
+				System.out.println(dist[cur]);
+				return;
+			}
+
+			for (int i = 0; i < 3; i++) {
+				if (i == 0) {
+					next = cur - 1;
+				} else if (i == 1) {
+					next = cur + 1;
+				} else {
+					next = cur * 2;
+				}
+
+				if (next >= 0 && next < dist.length && dist[next] == -1) {
+					que.add(next);
+					dist[next] = dist[cur] + 1;
+				}
+			}
+
+		}
+	}
 }
