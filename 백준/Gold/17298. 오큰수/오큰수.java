302fb4912a418637b84ad7198af21c79ae23d7b3
박수현 <80260557+34suuuuu@users.noreply.github.com>
2024-12-07 18:41:11 +0900

    [Gold IV] Title: 오큰수, Time: 848 ms, Memory: 143852 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,34 +1,44 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
+	static int n;
+	static int[] nums;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
 
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
+		n = Integer.parseInt(st.nextToken());
+		nums = new int[n];
 
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+			nums[i] = Integer.parseInt(st.nextToken());
 		}
 
-		Stack<Integer> stack = new Stack<>();
+		Stack<Integer> stack = new Stack<Integer>();
 		for (int i = 0; i < n; i++) {
-			while(!stack.isEmpty() && arr[stack.peek()] < arr[i]){
-				arr[stack.pop()] = arr[i];
+			while (!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
+				nums[stack.pop()] = nums[i];
 			}
 			stack.push(i);
 		}
 
-		while(!stack.isEmpty()){
-			arr[stack.pop()] = -1;
+		while (!stack.isEmpty()) {
+			nums[stack.pop()] = -1;
 		}
-		for (int i : arr) {
-			sb.append(i).append(" ");
+
+		for (int j = 0; j < n; j++) {
+			sb.append(nums[j]).append(' ');
 		}
 		System.out.println(sb);
+
 	}
+
+
 }
