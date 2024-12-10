55bb899a4afff809d847149a5bc9ba5ed55d9f4e
박수현 <80260557+34suuuuu@users.noreply.github.com>
2024-12-10 15:33:35 +0900

    [Silver I] Title: 스티커, Time: 692 ms, Memory: 109748 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -1,34 +1,40 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		int t = Integer.parseInt(br.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test_case = Integer.parseInt(br.readLine());
 
-		while (t-- > 0) {
+		for (int t = 0; t < test_case; t++) {
 			int n = Integer.parseInt(br.readLine());
+			int[][] stickers = new int[2][n];
 
-			// 스티커 초기화
-			int[][] stickers = new int[2][n + 1];
-			int[][] dp = new int[2][n + 1];
 			for (int i = 0; i < 2; i++) {
-				st = new StringTokenizer(br.readLine());
-				for (int j = 1; j <= n; j++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < n; j++) {
 					stickers[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
 
-			dp[0][1] = stickers[0][1];
-			dp[1][1] = stickers[1][1];
+			int[][] dp = new int[2][n];
+			dp[0][0] = stickers[0][0];
+			dp[1][0] = stickers[1][0];
 
-			for (int j = 2; j <= n; j++) {
-				dp[0][j] = Math.max(dp[1][j - 1], dp[1][j - 2]) + stickers[0][j];
-				dp[1][j] = Math.max(dp[0][j - 1], dp[0][j - 2]) + stickers[1][j];
+			for (int i = 1; i < n; i++) {
+				if (i == 1) {
+					dp[0][i] = dp[1][0] + stickers[0][1];
+					dp[1][i] = dp[0][0] + stickers[1][1];
+					continue;
+				}
+				dp[0][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + stickers[0][i];
+				dp[1][i] = Math.max(dp[0][i - 1], dp[0][i - 2]) + stickers[1][i];
 			}
-			sb.append(Math.max(dp[0][n], dp[1][n])).append("\n");
+
+			sb.append(Math.max(dp[0][n - 1], dp[1][n - 1])).append("\n");
 		}
 		System.out.println(sb);
 	}
