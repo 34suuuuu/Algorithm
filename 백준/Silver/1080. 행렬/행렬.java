245e5328ce9cc244919a3aa73ade17cc9edf410e
박수현 <80260557+34suuuuu@users.noreply.github.com>
2024-12-25 11:31:55 +0900

    [Silver I] Title: 행렬, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/1080. 행렬/행렬.java
+++ 백준/Silver/1080. 행렬/행렬.java
@@ -1,53 +1,64 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+	static int[][] result;
+	static int[][] original;
+	static int n, m;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
 
-		int n = Integer.parseInt(stringTokenizer.nextToken());
-		int m = Integer.parseInt(stringTokenizer.nextToken());
+		result = new int[n][m];    // 결과 행렬
+		original = new int[n][m];    // 원본 행렬
+
+		for (int i = 0; i < n; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < m; j++) {
+				original[i][j] = str.charAt(j) - '0';
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < m; j++) {
+				result[i][j] = str.charAt(j) - '0';
+			}
+		}
+
 		int cnt = 0;
-
-		char[][] matrixA = new char[n][m];
-		char[][] matrixB = new char[n][m];
-
-		for(int i = 0 ; i < n ; i++){
-			String str = br.readLine();
-			matrixA[i] = str.toCharArray();
-		}
-
-		for(int i = 0 ; i < n ; i++){
-			String str = br.readLine();
-			matrixB[i] = str.toCharArray();
-		}
-
-		for(int i = 0 ; i <= n-3 ; i++){
-			for(int j = 0; j <= m -3 ; j++){
-				if(matrixA[i][j] != matrixB[i][j]) {
-					swap(matrixA, i, j);
+		for (int i = 0; i < n - 2; i++) {
+			for (int j = 0; j < m - 2; j++) {
+				if (original[i][j] != result[i][j]) {
+					chaneNum(i, j);
 					cnt++;
 				}
 			}
 		}
+		System.out.println(chkEqual() ? cnt : -1);
+	}
 
-		for(int i = 0 ; i < n ; i++){
-			for(int j = 0; j < m; j++){
-				if(matrixA[i][j] != matrixB[i][j]){
-					System.out.println(-1);
-					return;
+	public static void chaneNum(int x, int y) {
+		for (int i = x; i < x + 3; i++) {
+			for (int j = y; j < y + 3; j++) {
+				original[i][j] = original[i][j] == 0 ? 1 : 0;
+			}
+		}
+	}
+
+	public static boolean chkEqual() {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (result[i][j] != original[i][j]) {
+					return false;
 				}
 			}
 		}
-		System.out.println(Arrays.deepEquals(matrixA, matrixB) ? cnt : -1);
-	}
-
-	static void swap(char[][] arr, int x, int y) {
-		for (int i = x; i < x + 3; i++) {
-			for (int j = y; j < y + 3; j++) {
-				arr[i][j] = arr[i][j] == '1' ? '0' : '1';
-			}
-		}
+		return true;
 	}
 }
