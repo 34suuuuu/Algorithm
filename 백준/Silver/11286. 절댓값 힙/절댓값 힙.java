1971f143d6bc7ee30414c7f43e2eb2d66b0241ea
박수현 <80260557+34suuuuu@users.noreply.github.com>
2025-01-11 14:42:05 +0900

    [Silver I] Title: 절댓값 힙, Time: 288 ms, Memory: 26176 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,32 +1,41 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+
 		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(new Comparator<Integer>() {
-			//	양수를 주면 순서를 바꾸지 않겠다
-			//	음수를 주면 순서를 바꾸겠다
+
 			@Override
 			public int compare(Integer o1, Integer o2) {
-				if (Math.abs(o1) > Math.abs(o2)) {
-					return Math.abs(o1) - Math.abs(o2);
-				} else if (Math.abs(o1) == Math.abs(o2)) {
+				if (Math.abs(o1) == Math.abs(o2)) {
 					return o1 - o2;
-				}else{
-					return -1;
+				}else if(Math.abs(o1) > Math.abs(o2)) {
+					return Math.abs(o1) - Math.abs(o2);
 				}
+				return -1;
 			}
 		});
 
-		for (int i = 0; i < N; i++) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++) {
 			int num = Integer.parseInt(br.readLine());
 			if (num != 0) {
 				pq.add(num);
 			} else {
-				System.out.println(!pq.isEmpty() ? pq.poll() : 0);
+				if (pq.isEmpty()) {
+					sb.append("0").append("\n");
+				} else {
+					sb.append(pq.poll()).append("\n");
+				}
 			}
 		}
+		System.out.println(sb);
+		
 	}
 }
