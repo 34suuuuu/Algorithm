cf40e98a25d4fd4d17595c379510bb56c93f73db
박수현 <80260557+34suuuuu@users.noreply.github.com>
2025-01-25 23:49:23 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1148 ms, Memory: 58516 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -1,33 +1,47 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+class Point implements Comparable<Point> {
+	int x, y;
+
+	Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public int compareTo(Point o) {
+		if (this.y == o.y) {
+			return this.x - o.x;
+		}
+		return this.y - o.y;
+	}
+}
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int[][] nums = new int[n][2];
 
-		
+		Point[] points = new Point[n];
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			nums[i][0] = Integer.parseInt(st.nextToken());
-			nums[i][1] = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+
+			points[i] = new Point(x, y);
 		}
 
-		Arrays.sort(nums, (o1, o2) ->{
-			if (o1[1] == o2[1]) {
-				return o1[0] - o2[0];
-			} else {
-				return o1[1] - o2[1];
-			}
-		});
+		Arrays.sort(points);
 
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < n; i++) {
-			sb.append(nums[i][0] + " " + nums[i][1]);
-			sb.append("\n");
+		for(Point point : points) {
+			System.out.println(point.x + " " + point.y);
 		}
-		System.out.println(sb);
+
 	}
 }
-
