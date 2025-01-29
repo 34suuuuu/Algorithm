ecd872e214e481d06e24cc4c7128e368028d926d
박수현 <80260557+34suuuuu@users.noreply.github.com>
2025-01-30 00:34:59 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 360 ms, Memory: 42924 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,47 +1,48 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
 
-		Stack<Character> stack;
 		while (true) {
-			stack = new Stack<>();
-			String str = br.readLine();
-			if (str.equals("."))
-				break;
+			String input = br.readLine();
+			if(input.equals(".")) break;
 
-			for (int i = 0; i < str.length(); i++) {
-				char c = str.charAt(i);
-
-				if (c == '(' || c == '[') {
-					stack.push(c);
-				} else if (c == ')') {
-					if (stack.isEmpty()) {
-						stack.push(c);
-					}
-					else if (stack.peek() == '(') {
-						stack.pop();
-					} else {
-						stack.push(c);
-					}
-				} else if (c == ']') {
-					if (stack.isEmpty()) {
-						stack.push(c);
-					}
-					else if (stack.peek() == '[') {
-						stack.pop();
-					} else {
-						stack.push(c);
-					}
+			Stack<String> stack = new Stack<>();
+			String[] str = input.strip().split("");
+			for (String s : str) {
+				switch (s) {
+					case "(":
+					case"[":
+						stack.push(s);
+						break;
+					case ")":
+						if (!stack.isEmpty() && stack.peek().equals("(")) {
+							stack.pop();
+						} else {
+							stack.push(s);
+						}
+						break;
+					case "]":
+						if (!stack.isEmpty() && stack.peek().equals("[")) {
+							stack.pop();
+						} else {
+							stack.push(s);
+						}
+						break;
 				}
 			}
-			sb.append(stack.isEmpty()?"yes":"no").append("\n");
+			if (stack.isEmpty()) {
+				sb.append("yes");
+			} else {
+				sb.append("no");
+			}
+			sb.append("\n");
 		}
 		System.out.println(sb);
-
 	}
 }
