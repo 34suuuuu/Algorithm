8dd51c07177700565c346ced50c7d5eb7d7bc3c3
박수현 <80260557+34suuuuu@users.noreply.github.com>
2025-02-07 16:58:59 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1132 ms, Memory: 57244 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,33 +1,43 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[][] nums = new int[n][2];
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		
+		int n = Integer.parseInt(st.nextToken());
+
+		List<int[]> list = new ArrayList<>();
 		for (int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			nums[i][0] = Integer.parseInt(st.nextToken());
-			nums[i][1] = Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine());
+			int[] tmp = new int[2];
+			tmp[0] = Integer.parseInt(st.nextToken());
+			tmp[1] = Integer.parseInt(st.nextToken());
+
+			list.add(tmp);
 		}
 
-		Arrays.sort(nums, (o1, o2) ->{
-			if (o1[0] == o2[0]) {
-				return o1[1] - o2[1];
-			} else {
+		Collections.sort(list, new Comparator<int[]> (){
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if (o1[0] == o2[0]) {
+					return o1[1] - o2[1];
+				}
 				return o1[0] - o2[0];
 			}
 		});
 
-		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < n; i++) {
-			sb.append(nums[i][0] + " " + nums[i][1]);
-			sb.append("\n");
+			System.out.println(list.get(i)[0] + " " + list.get(i)[1]);
 		}
-		System.out.println(sb);
+
 	}
 }
-
