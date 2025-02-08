6b6d925a12721f6fa25fa745c0976b909ff92cc3
박수현 <80260557+34suuuuu@users.noreply.github.com>
2025-02-08 12:56:00 +0900

    [Silver IV] Title: 듣보잡, Time: 268 ms, Memory: 26300 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,31 +1,38 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		Map<String, Integer> people = new HashMap<>();
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		for (int i = 0; i < n; i++) {
-			String name = br.readLine();
-			people.put(name,people.getOrDefault(name,0)+1);
-		}
-		List<String> names = new LinkedList<>();
-		for (int i = 0; i < m; i++) {
-			String name = br.readLine();
-			if (people.containsKey(name))
-				names.add(name);
-		}
-		Collections.sort(names);
-		StringBuilder sb = new StringBuilder();
-		sb.append(names.size()).append("\n");
-		for(String name : names) {
-			sb.append(name).append("\n");
-		}
 
-		System.out.println(sb);
+		HashSet<String> people = new HashSet<>();
+		List<String> result = new ArrayList<>();
+
+		for (int i = 0; i < n; i++) {
+			people.add(br.readLine());
+		}
+		for (int i = 0; i < m; i++) {
+			String person = br.readLine();
+			if (people.contains(person)) {
+				result.add(person);
+			}
+		}
+		Collections.sort(result);
+
+		System.out.println(result.size());
+		for (String s : result) {
+			System.out.println(s);
+		}
 	}
+
 }
