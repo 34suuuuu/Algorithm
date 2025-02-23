252427e3ca6e7fc91edcc78a3e0288b5d1803e59
박수현 <80260557+34suuuuu@users.noreply.github.com>
2025-02-23 09:14:17 +0900

    [Silver V] Title: 소트인사이드, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,15 +1,17 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
-		char[] strToArray = sc.nextLine().toCharArray();
-		Arrays.sort(strToArray);
-		StringBuilder sb = new StringBuilder(new StringBuilder(String.valueOf(strToArray)));
-		String result = "";
-		result = sb.reverse().toString();
-		System.out.println(result);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split("");
+		Arrays.sort(input, Comparator.reverseOrder());
+		for(String s : input) {
+			System.out.print(s);
+		}
 
 	}
 }
