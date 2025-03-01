a7ceac2e4d437b4481fec3afa53a3e67e773f140
박수현 <80260557+34suuuuu@users.noreply.github.com>
2025-03-01 22:13:01 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14080 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,31 +1,34 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		boolean[][] visited = new boolean[100][100];
-		int[][] papers = new int[100][100];
-
 		int n = Integer.parseInt(br.readLine());
-		int result = 0;
 
+		int[][] paper = new int[100][100];
 		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-
-			for (int x = a; x < a + 10; x++) {
-				for (int y = b; y < b + 10; y++) {
-					if (!visited[x][y]) {
-						visited[x][y] = true;
-						result++;
-					}
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			for (int j = x; j < x + 10; j++) {
+				for (int k = y; k < y + 10; k++) {
+					paper[j][k] = 1;
 				}
 			}
 		}
 
+		int result = 0;
+		for (int i = 0; i < 100; i++) {
+			for (int j = 0; j < 100; j++) {
+				if (paper[i][j] == 1) {
+					result++;
+				}
+			}
+		}
 		System.out.println(result);
 	}
+
 }
