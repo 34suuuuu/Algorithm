6fe6fb83ee3c1cb52202be7047663e14c9f8ce3e
박수현 <80260557+34suuuuu@users.noreply.github.com>
2025-04-09 09:03:03 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.25 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,18 +1,16 @@
 import java.util.*;
+
 class Solution {
-    public int[] solution(int n, String[] words) {
-        int[] answer = new int[2];  // 번호, 차례
-    
-        Set<String> set = new HashSet<>();
-        set.add(words[0]);
+    public int[] solution(int n, String[] words) {    
+        List<String> list = new ArrayList<>();
+        list.add(words[0]);
         
         for(int i=1 ; i < words.length; i++){
-
-            if(set.contains(words[i]) || words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)){
+            if(list.contains(words[i]) || words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)){
                 return new int[]{(i % n) + 1,(i / n) + 1};
 
             }
-            set.add(words[i]);
+            list.add(words[i]);
         }
 
         return new int[]{0,0};
