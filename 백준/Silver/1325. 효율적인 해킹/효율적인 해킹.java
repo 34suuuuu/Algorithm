387d5fb3a2982f2c62faf42f2b6d45cc78d78cdc
박수현 <80260557+34suuuuu@users.noreply.github.com>
2025-04-18 11:18:08 +0900

    [Silver I] Title: 효율적인 해킹, Time: 9924 ms, Memory: 144460 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -2,14 +2,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 	static List<Integer>[] maps;
 	static boolean[] visited;
-	static int[] answer;
+	static int cnt = 0;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,8 +17,6 @@ public class Main {
 
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-
-		answer = new int[n + 1];
 
 		// 신뢰 관계 초기화
 		maps = new ArrayList[n + 1];
@@ -30,20 +28,24 @@ public class Main {
 			st = new StringTokenizer(br.readLine());
 			int com1 = Integer.parseInt(st.nextToken());
 			int com2 = Integer.parseInt(st.nextToken());
-			maps[com1].add(com2);
+			maps[com2].add(com1);
 		}
 
+		ArrayList<Integer> count = new ArrayList<>();
 		for (int i = 1; i <= n; i++) {
 			// 방문 배열 초기화
 			visited = new boolean[n + 1];
 			dfs(i);
+			// 해킹할 수 있는 컴퓨터 갯수
+			count.add(cnt);
+			cnt = 0;
 		}
 
-		int maxValue = Arrays.stream(answer).max().getAsInt();
+		int maxValue = Collections.max(count);
 		StringBuilder sb = new StringBuilder();
-		for (int i = 1; i <= n; i++) {
-			if (answer[i] == maxValue) {
-				sb.append(i).append(" ");
+		for (int i = 0; i < n; i++) {
+			if (count.get(i) == maxValue) {
+				sb.append(i + 1).append(" ");
 			}
 		}
 		System.out.println(sb);
@@ -54,11 +56,9 @@ public class Main {
 
 		for (int com : maps[n]) {
 			if (!visited[com]) {
-				visited[com] = true;
-				answer[com]++;
+				cnt++;
 				dfs(com);
 			}
 		}
-
 	}
 }
