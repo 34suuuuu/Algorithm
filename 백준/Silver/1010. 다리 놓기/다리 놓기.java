2c1a5c6c3c95750619123c9456350abf3cbe1480
박수현 <80260557+34suuuuu@users.noreply.github.com>
2025-04-22 11:19:20 +0900

    [Silver V] Title: 다리 놓기, Time: 108 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[][] dp;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+
+		dp = new int[30][30];
+		StringBuilder sb = new StringBuilder();
+		while (t-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+
+			sb.append(combination(m, n)).append("\n");
+		}
+		System.out.println(sb);
+
+	}
+
+	static int combination(int n, int r) {
+
+		if(dp[n][r] > 0) {
+			return dp[n][r];
+		}
+
+		if(n == r || r == 0) {
+			return dp[n][r] = 1;
+		}
+
+		return dp[n][r] = combination(n-1, r-1) + combination(n-1, r);
+	}
+}
