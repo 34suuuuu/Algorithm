172279adc996d0349b1e42ab1aab21a40c451639
박수현 <80260557+34suuuuu@users.noreply.github.com>
2025-04-27 14:42:16 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 260 ms, Memory: 47400 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -1,58 +1,78 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static char map[][];
-    public static boolean visited[][];
-    public static int dx[] = {0, 0, 1, -1};
-    public static int dy[] = {1, -1, 0, 0};
-    public static int n, m, cnt;
-    public static int cx, cy;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+	static int n, m, meetingPerson = 0;
+	static char[][] maps;
+	static int[] dx = {1, 0, -1, 0};
+	static int[] dy = {0, 1, 0, -1};
+	static boolean[][] visited;
 
-        map = new char[n][m];
-        visited = new boolean[n][m];
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            String s = st.nextToken();
-            for (int j = 0; j < m; j++) {
-                if(s.charAt(j)=='I'){
-                    cx=i;
-                    cy=j;
-                }
-                map[i][j] = s.charAt(j);
-            }
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        dfs(cx,cy);
-        check();
-    }
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		maps = new char[n][m];
+		visited = new boolean[n][m];
 
-    public static void dfs(int x, int y) {
-        visited[x][y] = true;
-        if(map[x][y]=='P') cnt++;
+		int start_x = 0, start_y = 0;
+		for (int i = 0; i < n; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < m; j++) {
+				maps[i][j] = str.charAt(j);
 
-        for (int i = 0; i < 4; i++) {
-            int cx = x + dx[i];
-            int cy = y + dy[i];
+				if (maps[i][j] == 'I') {// 도연이의 초기 위치
+					start_x = i;
+					start_y = j;
+				}
+			}
+		}
+		dfs(start_x, start_y);
+		System.out.println(meetingPerson == 0 ? "TT" : meetingPerson);
 
-            if (cx >= 0 && cy >= 0 && cx < n && cy < m ) {
-                if (!visited[cx][cy] && map[cx][cy] != 'X') {
-                    dfs(cx, cy);
-                }
-            }
-        }
-    }
+	}
 
-    public static void check() {
-        if (cnt <= 0) {
-            System.out.println("TT");
-        } else {
-            System.out.println(cnt);
-        }
-    }
+	// private static void dfs(int x, int y) {
+	// 	visited[x][y] = true;
+	//
+	// 	if (maps[x][y] == 'P') {
+	// 		meetingPerson++;
+	// 	}
+	//
+	// 	for (int i = 0; i < 4; i++) {
+	// 		int nx = x + dx[i];
+	// 		int ny = y + dy[i];
+	//
+	// 		if(nx < 0 || nx >= n || ny < y || ny >= m || visited[nx][ny] || maps[nx][ny] == 'X') continue;
+	//
+	// 		if (nx > 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny]) {
+	// 			// 범위내에 있는 방문하지 않은 지점
+	//
+	// 			if (maps[nx][ny] != 'X') {
+	// 				dfs(nx, ny);
+	// 			}
+	// 		}
+	//
+	// 		dfs(nx, ny);
+	// 	}
+	// }
+	public static void dfs(int x, int y) {
+		visited[x][y] = true;
+		if(maps[x][y]=='P') meetingPerson++;
+
+		for (int i = 0; i < 4; i++) {
+			int cx = x + dx[i];
+			int cy = y + dy[i];
+
+			if (cx >= 0 && cy >= 0 && cx < n && cy < m ) {
+				if (!visited[cx][cy] && maps[cx][cy] != 'X') {
+					dfs(cx, cy);
+				}
+			}
+		}
+	}
 }
