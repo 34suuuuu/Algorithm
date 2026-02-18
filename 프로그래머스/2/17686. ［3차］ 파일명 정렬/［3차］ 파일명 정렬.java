712af40430f58ed8a149797c4865c9b2470cf47a
박수현 <80260557+34suuuuu@users.noreply.github.com>
2026-02-18 22:15:25 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 28.99 ms, Memory: 98.9 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,27 +1,48 @@
 import java.io.*;
 import java.util.*;
 
+class File implements Comparable<File>{
+    String head, number;
+    int idx;
+    
+    File(String head, String number, int idx){
+        this.head = head;
+        this.number = number;
+        this.idx = idx;
+    }
+    
+    @Override
+    public int compareTo(File o){
+        String head1 = this.head.toLowerCase();
+        String head2 = o.head.toLowerCase();
+        int headCmp = head1.compareTo(head2);
+        if(headCmp != 0)return headCmp;
+        
+        int num1 = Integer.parseInt(this.number);
+        int num2 = Integer.parseInt(o.number);
+        return Integer.compare(num1, num2);
+    }
+    
+    
+}
+
 class Solution {
-    public String[] solution(String[] files) {                
-        Arrays.sort(files, (a, b) -> {
-            String[] s1 = splitFile(a);
-            String[] s2 = splitFile(b);
+    public String[] solution(String[] files) {   
+        List<File> result = new ArrayList<>();
+        
+        for(int i = 0; i < files.length; i++){
+            String[] file = splitFile(files[i]);
+            result.add(new File(file[0], file[1], i));
+        }
+        Collections.sort(result);
+        
+        String[] answer = new String[files.length];
+        for(int i = 0; i < files.length; i++){
+            answer[i] = files[result.get(i).idx];
             
-            // 1. 대소문자 구분없이 head 오름차순
-            String head1 = s1[0].toLowerCase();
-            String head2 = s2[0].toLowerCase();
-            int headCmp = head1.compareTo(head2);
-            
-            if(headCmp != 0){
-                return headCmp;
-            }
-            
-            // 2. number 오름차순
-            int num1 = Integer.parseInt(s1[1]);
-            int num2 = Integer.parseInt(s2[1]);
-            return Integer.compare(num1, num2); 
-        });
-        return files;
+        }
+        return answer;
+       
     }
     
     public String[] splitFile(String s){
