64d79135ac640ff2ee7965cf37248e8e17b6fe28
박수현 <80260557+34suuuuu@users.noreply.github.com>
2026-04-27 20:04:25 +0900

    [Silver IV] Title: 등수 구하기, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.java
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.java
@@ -1,11 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int n = Integer.parseInt(st.nextToken());
@@ -13,25 +14,22 @@ public class Main {
 		int p = Integer.parseInt(st.nextToken());
 
 		int[] scores = new int[n];
-		if(n > 0){
+		int rank = 1;
+		if (n > 0) {
 			st = new StringTokenizer(br.readLine());
-			for(int i = 0; i < n; i++){
+			for (int i = 0; i < n; i++) {
 				scores[i] = Integer.parseInt(st.nextToken());
-			}
-		}
 
-		if(n == p && scores[n-1] >= score){
-			System.out.println(-1);
-			return;
-		}
-		else{
-			int rank = 1;
-			for(int i = 0; i < n; i++){
-				if(scores[i] > score){
+				if (scores[i] > score) {
 					rank++;
-				}else break;
+				}
 			}
-			System.out.println(rank);
+
+			if (score <= scores[n - 1] && n == p) {
+				System.out.println(-1);
+				return;
+			}
 		}
+		System.out.println(rank);
 	}
 }
