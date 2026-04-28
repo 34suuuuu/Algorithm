485b40e937fef3f765fa5009ae083cf22950bb1d
박수현 <80260557+34suuuuu@users.noreply.github.com>
2026-04-28 23:32:23 +0900

    [level 2] Title: 피로도, Time: 9.40 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,34 +2,33 @@ import java.util.*;
 
 class Solution {
     static boolean[] visited;
-    static List<Integer> result;
-    static Set<Integer> resultSet = new HashSet<>();
+    static HashSet<Integer> nums = new HashSet<>();
     
     public int solution(int k, int[][] dungeons) {
-        int answer = -1;
-        
+        int answer = 0;
         visited = new boolean[dungeons.length];
-		int cnt = 0;
-        
-		dfs(k, 0, cnt, dungeons);
-		answer = Integer.MIN_VALUE;
-		for (int i : resultSet) {
-			answer = Math.max(i, answer);
-		}
 
+        int cnt = 0;
+        dfs(k, 0, cnt, dungeons);
+        
+        for(Integer num : nums){
+            answer = Math.max(num, answer);
+        }
         
         return answer;
     }
     
-    static void dfs(int k, int start, int cnt, int[][] dungeons) {
-		for (int i = 0; i < dungeons.length; i++) {
-			if (!visited[i] && k >= dungeons[i][0]) {
-				visited[i] = true;
-				resultSet.add(cnt + 1);
-				dfs(k - dungeons[i][1], i + 1, cnt + 1, dungeons);
-				visited[i] = false;
-			}
-		}
-
-	}
+    private static void dfs(int k, int idx, int cnt, int[][] dungeons){
+        for(int i=0; i<dungeons.length; i++){
+            if(!visited[i] && k >= dungeons[i][0]){
+                // 방문하지 않았고, 최소필요피로도보다 현재가 큰 상태
+                visited[i] = true;
+                nums.add(cnt + 1);
+                dfs(k - dungeons[i][1], idx + 1, cnt + 1, dungeons);
+                visited[i] = false;
+            }
+        }
+    }
+    
+   
 }
