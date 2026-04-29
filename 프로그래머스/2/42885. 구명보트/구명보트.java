b329d8bbabb0e39191505dd08a2cdf0094d33d62
박수현 <80260557+34suuuuu@users.noreply.github.com>
2026-04-29 12:13:39 +0900

    [level 2] Title: 구명보트, Time: 31.80 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,27 +1,23 @@
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
-
+import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        
         Deque<Integer> deque = new ArrayDeque<>();
-		Arrays.sort(people);
-
-		for (int i : people) {
-			deque.add(i);
-		}
-
+        
+        Arrays.sort(people);
+        
+        for(int i : people){
+            deque.add(i);
+        }
+        
 		while(!deque.isEmpty()) {
-			int last = deque.pollLast();
-			answer++;
-
-			if (deque.size() >= 1 && last + deque.peekFirst() <= limit) {
-				deque.pollFirst();
-			}
-
-		}
+            answer++;
+            
+            int max = deque.pollLast();
+            if(!deque.isEmpty() && deque.peekFirst() + max <= limit){
+                deque.pollFirst();
+            }
+        }
         return answer;
     }
 }
